eb1c540396f32a37b3d9dd0538b2156fae762177
SantiagoPrada2005 <santiagopradamoreno@gmail.com>
2025-09-27 17:11:58 -0500

    feat(admin): add custom routes and product widget
    - Create nested custom route page with sidebar integration
    - Add product widget for product details section
    - Implement base custom route with sidebar icon and label

---
+++ cool-bordados/src/admin/routes/custom/page.tsx
@@ -0,0 +1,24 @@
+import { defineRouteConfig } from "@medusajs/admin-sdk"
+import { ChatBubbleLeftRight } from "@medusajs/icons"
+import { Container, Heading } from "@medusajs/ui"
+
+const CustomPage = () => {
+  return (
+    <Container className="divide-y p-0">
+      <div className="flex items-center justify-between px-6 py-4">
+        <Heading level="h2">This is my custom route</Heading>
+      </div>
+    </Container>
+  )
+}
+
+
+// Esta configuracion hace que aparezca la ruta creada en el sidebar
+export const config = defineRouteConfig({
+  label: "Custom Route",
+  icon: ChatBubbleLeftRight,
+})
+
+
+// The route
+export default CustomPage
